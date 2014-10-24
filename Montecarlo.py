c44d11ab9bf7665ace95058686ed189d60f5c12c
lorenzocatani <tedelori@libero.it>
2014-10-24 17:20:52 +0100

    Define energy function

--- Montecarlo.py
+++ Montecarlo.py
@@ -1,1 +1,12 @@
 from numpy import array, any, sum, exp, log
+
+def energy(f,density):
+  density = array(density)
+  if density.dtype.kind != 'i' and len(density) > 0:
+    raise TypeError ("Expected array of *integers*.")
+  if any (density < 0):
+    raise ValueError ("Expected array of *positive* integers.")
+  E=0
+  for element in density:
+	   E = f(element) + E   
+  return E
