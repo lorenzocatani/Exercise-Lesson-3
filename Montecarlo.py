4f26928a35daa4d4f62dbad2f296f3dbe393c7a8
lorenzocatani <tedelori@libero.it>
2014-10-24 17:24:40 +0100

    Define ChangeParticle

--- Montecarlo.py
+++ Montecarlo.py
@@ -10,3 +10,14 @@ def energy(f,density):
   for element in density:
 	   E = f(element) + E   
   return E
+
+def ChangeParticle(density,i,direction):
+  density = array(density)
+  
+  density[i] = density[i]-1 # Move a particle from the i-th position.
+  d=len(density) 
+  if i==d-1 and direction==1: density[0]=density[0]+1
+  elif i==0 and direction==0: density[d-1]=density[d-1]+1
+  elif direction==0 : density[i-1]= density[i-1]+1 # Particle moved on the left
+  else : density[i+1]=density[i+1]+1 # Particle moved on the right
+  return density
